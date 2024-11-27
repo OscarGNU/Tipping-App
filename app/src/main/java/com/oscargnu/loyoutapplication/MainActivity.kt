33a96f36648ccb6dc2021969f48234392dbf26a3
oscar <oscareduardozambrano@gmail.com>
2024-11-27 16:56:57 +0000

    Modification of the UI Calculation of tips according to percentage 10, 15 or 20

--- app/src/main/java/com/oscargnu/loyoutapplication/MainActivity.kt
+++ app/src/main/java/com/oscargnu/loyoutapplication/MainActivity.kt
@@ -1,25 +1,61 @@
 package com.oscargnu.loyoutapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputEditText
+import com.oscargnu.loyoutapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val btnClean = findViewById<Button>(R.id.btn_clean)
-        val btnDone = findViewById<Button>(R.id.btn_done)
-        val edtTotal =findViewById<TextInputEditText>(R.id.tie_total)
-        val edtNumPeople = findViewById<TextInputEditText>(R.id.tie_num_people)
 
-        btnClean.setOnClickListener{
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            var percentage: Int = 0
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+
+            if (isChecked){
+                percentage = 10
+            }
+        }
+
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked){
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+
+            if (isChecked){
+                percentage = 20
+            }
+
+        }
+
+
+
+        binding.btnClean.setOnClickListener{
+
+            println ("oscar " + binding.tieTotal.text)
+            println ("oscar1 " + binding.tieNumPeople.text)
+        }
+
+        binding.btnDone.setOnClickListener{
+            val totalTable: Float = binding.tieTotal.text.toString().toFloat()
+            val nPeople: Int = binding.tieNumPeople.text.toString().toInt()
+
+            val totalTemp = totalTable / nPeople
+            val tips = totalTemp * percentage / 100
+            val totalWithTips = totalTemp + tips
+
+            binding.tvResult.text = "Total with tips: $totalWithTips"
+
+
         }
 
     }
